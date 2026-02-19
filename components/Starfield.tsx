9efb0bcf6d87726d9b24e64f572c4e6f830adcd7
Verspot <verspot@163.com>
2026-02-20 07:29:12 +0800

    Add planets to starfield

--- components/Starfield.tsx
+++ components/Starfield.tsx
@@ -53,12 +53,91 @@ const Starfield: React.FC = () => {
       }
     }
 
-    // Create stars
+    // Planet class
+    class Planet {
+      x: number;
+      y: number;
+      radius: number;
+      speedX: number;
+      speedY: number;
+      colors: string[];
+      glowColor: string;
+      rotation: number;
+      rotationSpeed: number;
+
+      constructor() {
+        this.radius = Math.random() * 30 + 20;
+        this.x = Math.random() * (canvas.width - this.radius * 2) + this.radius;
+        this.y = Math.random() * (canvas.height - this.radius * 2) + this.radius;
+        this.speedX = (Math.random() - 0.5) * 0.3;
+        this.speedY = (Math.random() - 0.5) * 0.3;
+        this.rotation = 0;
+        this.rotationSpeed = (Math.random() - 0.5) * 0.02;
+
+        const colorPalettes = [
+          ['#4F46E5', '#7C3AED', '#8B5CF6'],
+          ['#06B6D4', '#0891B2', '#0E7490'],
+          ['#EC4899', '#DB2777', '#BE185D'],
+          ['#F59E0B', '#D97706', '#B45309'],
+          ['#10B981', '#059669', '#047857'],
+          ['#6366F1', '#4F46E5', '#4338CA']
+        ];
+        this.colors = colorPalettes[Math.floor(Math.random() * colorPalettes.length)];
+        this.glowColor = this.colors[0];
+      }
+
+      update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        this.rotation += this.rotationSpeed;
+
+        if (this.x < -this.radius) this.x = canvas.width + this.radius;
+        if (this.x > canvas.width + this.radius) this.x = -this.radius;
+        if (this.y < -this.radius) this.y = canvas.height + this.radius;
+        if (this.y > canvas.height + this.radius) this.y = -this.radius;
+      }
+
+      draw() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.rotation);
+
+        const gradient = ctx.createRadialGradient(0, 0, 0, 0, 0, this.radius);
+        gradient.addColorStop(0, this.colors[0]);
+        gradient.addColorStop(0.5, this.colors[1]);
+        gradient.addColorStop(1, this.colors[2]);
+
+        ctx.fillStyle = gradient;
+        ctx.beginPath();
+        ctx.arc(0, 0, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+
+        ctx.restore();
+
+        const glowGradient = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.radius * 2);
+        glowGradient.addColorStop(0, this.glowColor + '40');
+        glowGradient.addColorStop(0.5, this.glowColor + '20');
+        glowGradient.addColorStop(1, this.glowColor + '00');
+
+        ctx.fillStyle = glowGradient;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius * 2, 0, Math.PI * 2);
+        ctx.fill();
+      }
+    }
+
+    // Create stars and planets
     const stars: Star[] = [];
+    const planets: Planet[] = [];
     const starCount = Math.floor(canvas.width * canvas.height / 10000);
+    const planetCount = Math.floor(Math.random() * 3) + 2;
 
     for (let i = 0; i < starCount; i++) {
       stars.push(new Star());
+    }
+
+    for (let i = 0; i < planetCount; i++) {
+      planets.push(new Planet());
     }
 
     // Animation loop
@@ -68,6 +147,11 @@ const Starfield: React.FC = () => {
       stars.forEach(star => {
         star.update();
         star.draw();
+      });
+
+      planets.forEach(planet => {
+        planet.update();
+        planet.draw();
       });
 
       requestAnimationFrame(animate);
